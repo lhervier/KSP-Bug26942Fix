0a9a1a7445b483f35c8e6e00a20c13b460f1ac12
lionel.hervier@laposte.net <lionel.hervier@laposte.net>
2021-06-28 23:44:45 +0200

    first commit

---
+++ AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle("Bug26942Fix")]
+[assembly: AssemblyDescription("Fix KSP Bug 26942")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.0.*")]
